fc5c801af369fa801c0a7b98aa480532a999cbcc
Bradley Newlon <Bradleynewlon@gmail.com>
2022-06-14 08:13:25 -0400

    form

--- components/SecondNav.js
+++ components/SecondNav.js
@@ -7,6 +7,11 @@ const SecondNav = () => {
         <div style={{ width: '100%', backgroundColor: 'whitesmoke', display: 'flex', textAlign: 'end', paddingRight: '2rem', paddingLeft: '2rem' }}>
             <ul style={{ listStyle: 'none', display: 'inline', textDecoration: 'none', width: '100%' }}>
                 <li>
+                    <Link href="/submit">
+                        Submit a new Class/Event
+                    </Link>
+                </li>
+                <li>
                     <Link href="/register">
                         Sign Up
                     </Link>
